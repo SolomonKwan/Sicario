270fb71e5a362be244cbd3c3a160014ae27eaf3f
SoloKwansy <solkwan@hotmail.com>
2021-01-10 10:20:08 +1000

    Updated test files and test suite.

--- tests/test.sh
+++ tests/test.sh
@@ -29,17 +29,32 @@ clean() {
 	echo "Removing files:"
 	echo "    sc_result_vd"
 	echo "    sf_result_vd"
-	rm /home/solomon/Sicario/tests/sc_result_vd /home/solomon/Sicario/tests/sf_result_vd
+	rm ~/Sicario/tests/sc_result_vd ~/Sicario/tests/sf_result_vd
 }
 
 record_result() {
-    echo "Recording result " "$1"
+    echo "Recording result" "$1"
+	local entry=$line
+	for i in {1..6}; do
+		local val=$(stockfish_value "$1" $i)
+		entry=$entry' ;D'$i' '$val
+	done
+	echo "$entry" >> perftsuite.epd
+}
+
+stockfish_value() {
+    ~/Stockfish/src/stockfish > ~/Sicario/tests/sf_result <<-EOF
+		position fen "$1"
+		go perft $2
+	EOF
+	echo $(cut -d ":" -f2- <<< $(tail -n 2 ~/Sicario/tests/sf_result))
 }
 
 record_file() {
     while IFS= read -r line; do
-        echo "$line"
+		record_result "$line"
     done < $1
+	rm ~/Sicario/tests/sf_result
 }
 
 perft() {
@@ -48,25 +63,25 @@ perft() {
 	echo "Testing perft $1 on " $FEN
 
 	# Run stockfish perft
-	/home/solomon/Stockfish/src/stockfish > /home/solomon/Sicario/tests/sf_result <<-EOF
+	~/Stockfish/src/stockfish > ~/Sicario/tests/sf_result <<-EOF
 		position fen $FEN
 		go perft $1
 	EOF
-	tail -n +2 /home/solomon/Sicario/tests/sf_result | sort > /home/solomon/Sicario/tests/sf_result_vd
-	rm /home/solomon/Sicario/tests/sf_result
+	tail -n +2 ~/Sicario/tests/sf_result | sort > ~/Sicario/tests/sf_result_vd
+	rm ~/Sicario/tests/sf_result
 
 	# Run sicario perft
-	make -C /home/solomon/Sicario/src/ -s
-	/home/solomon/Sicario/src/sicario > /home/solomon/Sicario/tests/sc_result <<-EOF
+	make -C ~/Sicario/src/ -s
+	~/Sicario/src/sicario > ~/Sicario/tests/sc_result <<-EOF
 		set fen $FEN
 		perft $1
 		exit
 	EOF
-	tail -n +21 /home/solomon/Sicario/tests/sc_result | sort > /home/solomon/Sicario/tests/sc_result_vd
-	rm /home/solomon/Sicario/tests/sc_result
+	tail -n +21 ~/Sicario/tests/sc_result | sort > ~/Sicario/tests/sc_result_vd
+	rm ~/Sicario/tests/sc_result
 
 	# Vimdiff the results
-	vimdiff /home/solomon/Sicario/tests/sc_result_vd /home/solomon/Sicario/tests/sf_result_vd
+	vimdiff ~/Sicario/tests/sc_result_vd ~/Sicario/tests/sf_result_vd
 }
 
 main "$@"
